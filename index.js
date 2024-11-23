8d00acc52edd7d347cb5a59e5727b6fd3b6796a5
chanchal-deore <chanchaldeore1@gmail.com>
2024-11-24 00:26:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+import express from "express";
+import dotenv from "dotenv";
+import router from "../routers/feedbackRouter.js";
+import dbConnect from "../config/database.js";
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = 3000;  
+
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+res.send("hello");
+}
+app.use("/api/v1", router);
+
+dbConnect();
+
+app.listen(PORT, () => {
+  console.log("Server is running at port: ", PORT);
+});
